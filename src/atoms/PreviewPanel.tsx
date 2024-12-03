879e7f35223c6d2275a2ce317cd0072a988d10cf
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-12-04 01:30:07 +0700

    add support for math

--- src/atoms/PreviewPanel.tsx
+++ src/atoms/PreviewPanel.tsx
@@ -5,7 +5,10 @@ import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 import remarkBreaks from "remark-breaks";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 import "highlight.js/styles/github-dark.css";
+import "katex/dist/katex.min.css";
 
 type Props = {
   code: string | undefined;
@@ -25,10 +28,11 @@ const PreviewPanel: React.FC<Props> = ({ code }) => {
       <h1 className="text-center text-5xl italic">Preview</h1>
       <div className="h-[88vh] overflow-scroll scroll-smooth scrollbar scrollbar-track-transparent scrollbar-thumb-white">
         <Markdown
-          remarkPlugins={[remarkGfm, remarkBreaks]}
+          remarkPlugins={[remarkGfm, remarkBreaks, remarkMath]}
           rehypePlugins={[
             rehypeHighlight,
             rehypeRaw,
+            rehypeKatex,
             [rehypeSanitize, customSchema],
           ]}
           className="font-sans text-lg"
